b86e3edeb712c6a953656a06bcb47f9a1960a6c0
Maksim Zaveniagin <Maksim_Zaveniagin@epam.com>
2021-06-22 16:41:40 +0400

    Add bridges calculating

--- Core/GraphAlgorithms.cs
+++ Core/GraphAlgorithms.cs
@@ -32,15 +32,60 @@ namespace Core
 
             return used == vertexMaskLimit - 1;
 
-            void Dfs(BigInteger prevVertexMask)
+            void Dfs(BigInteger vertexMask)
             {
-                used |= prevVertexMask;
+                used |= vertexMask;
 
-                for (BigInteger i = 1; i < vertexMaskLimit; i <<= 1)
+                for (BigInteger toMask = 1; toMask < vertexMaskLimit; toMask <<= 1)
                 {
-                    if ((used & i) == 0 && (graph[_numbersByMasks[i]] & i) != 0)
+                    if ((used & toMask) == 0 && (graph[_numbersByMasks[toMask]] & toMask) != 0)
                     {
-                        Dfs(i);
+                        Dfs(toMask);
+                    }
+                }
+            }
+        }
+
+        public static (BigInteger fromMask, BigInteger toMask)[] GetAllBridges(BigInteger[] graph)
+        {
+            BigInteger vertexMaskLimit = new BigInteger(1) << graph.Length;
+            BigInteger used = 0;
+            int[] timeOfIn = new int[graph.Length];
+            int[] dynamicTime = new int[graph.Length];
+            int currentTime = 0;
+
+            List<(BigInteger fromMask, BigInteger toMask)> result = new List<(BigInteger fromMask, BigInteger toMask)>();
+
+            Dfs(1, -1);
+
+            return result.ToArray();
+
+            void Dfs(BigInteger vertexMask, BigInteger prevVertexMask)
+            {
+                used |= vertexMask;
+                timeOfIn[_numbersByMasks[vertexMask]] = dynamicTime[_numbersByMasks[vertexMask]] = currentTime++;
+
+                for (BigInteger toMask = 1; toMask < vertexMaskLimit; toMask <<= 1)
+                {
+                    if (toMask == prevVertexMask)
+                    {
+                        continue;
+                    }
+
+                    if ((used & toMask) != 0)
+                    {
+                        dynamicTime[_numbersByMasks[vertexMask]] = Math.Min(dynamicTime[_numbersByMasks[vertexMask]], timeOfIn[_numbersByMasks[toMask]]);
+                    }
+                    else
+                    {
+                        Dfs(toMask, vertexMask);
+
+                        dynamicTime[_numbersByMasks[vertexMask]] = Math.Min(dynamicTime[_numbersByMasks[vertexMask]], dynamicTime[_numbersByMasks[toMask]]);
+
+                        if (dynamicTime[_numbersByMasks[toMask]] > timeOfIn[_numbersByMasks[vertexMask]])
+                        {
+                            result.Add((vertexMask, toMask));
+                        }
                     }
                 }
             }
