4887c2a71dedc372673d2203b2c1b296f0d5019c
Maksim Zaveniagin <Maksim_Zaveniagin@epam.com>
2021-06-22 16:22:10 +0400

    Add check connectivity

---
+++ Core/GraphAlgorithms.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Core
+{
+    public class GraphAlgorithms
+    {
+        private const int VertexLimit = 1000;
+        private static readonly Dictionary<BigInteger, int> _numbersByMasks;
+
+        static GraphAlgorithms()
+        {
+            _numbersByMasks = new Dictionary<BigInteger, int>();
+
+            BigInteger mask = 1;
+
+            for (int i = 0; i < VertexLimit; i++)
+            {
+                _numbersByMasks.Add(mask, i);
+
+                mask <<= 1;
+            }
+        }
+
+        public static bool IsConnected(BigInteger[] graph)
+        {
+            BigInteger vertexMaskLimit = new BigInteger(1) << graph.Length;
+            BigInteger used = 0;
+
+            Dfs(1);
+
+            return used == vertexMaskLimit - 1;
+
+            void Dfs(BigInteger prevVertexMask)
+            {
+                used |= prevVertexMask;
+
+                for (BigInteger i = 1; i < vertexMaskLimit; i <<= 1)
+                {
+                    if ((used & i) == 0 && (graph[_numbersByMasks[i]] & i) != 0)
+                    {
+                        Dfs(i);
+                    }
+                }
+            }
+        }
+    }
+}
